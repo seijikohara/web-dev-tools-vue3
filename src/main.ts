6740a07a6c7fd483026b9435394835af6f505f22
Seiji Kohara <sage.0118@gmail.com>
2023-08-26 11:41:36 +0900

    feat: change ga plugin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import VueGtag from 'vue-gtag-next'
+import VueGtag from 'vue-gtag'
 import PrimeVue from 'primevue/config'
 
 import 'primeflex/primeflex.css'
@@ -21,11 +21,16 @@ app.use(PrimeVue, { ripple: true, inputStyle: 'outlined' })
 
 const gaMeasurementId = import.meta.env.VUE_APP_GA_MEASUREMENT_ID
 if (gaMeasurementId) {
-  app.use(VueGtag, {
-    property: {
-      id: gaMeasurementId,
+  app.use(
+    VueGtag,
+    {
+      pageTrackerScreenviewEnabled: true,
+      config: {
+        id: gaMeasurementId,
+      },
     },
-  })
+    router,
+  )
 }
 
 app.mount('#app')
